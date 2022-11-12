ad02b19b61d71452f2aa3170690e3f782c1fd67b
Tao Feng <fengtao321@users.noreply.github.com>
2022-11-12 00:56:59 -0500

    multiple threads in Java

---
+++ 1195-fizz-buzz-multithreaded.java
@@ -0,0 +1,50 @@
+import java.util.function.IntConsumer;
+import java.util.function.Supplier;
+
+class FizzBuzz {
+    private int n;
+    private int counter;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+        this.counter = 1;
+    }
+
+    private synchronized void outputString(IntConsumer func, Supplier<Boolean> condition) throws InterruptedException {
+        while (this.counter <= this.n) {
+            if (condition.get()) {
+                func.accept(this.counter);
+                this.counter++;
+                this.notifyAll();
+            } else {
+                this.wait();
+            }
+        }
+
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        this.outputString(num -> printFizz.run(), () -> this.counter % 3 == 0 && this.counter % 5 != 0);
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        this.outputString(num -> printBuzz.run(), () -> this.counter % 5 == 0 && this.counter % 3 != 0);
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        this.outputString(num -> printFizzBuzz.run(), () -> this.counter % 5 == 0 && this.counter % 3 == 0);
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        this.outputString(printNumber, () -> this.counter % 5 != 0 && this.counter % 3 != 0);
+    }
+}
+
+// review CountDownLatch, ReentrantLock
+// CountDownLatch example: https://www.jianshu.com/p/205a61af1205
+// Semaphore：
+// https://www.cnblogs.com/jiading/articles/12519704.html#:~:text=Semaphore%20%E6%98%AF%E7%94%A8%E6%9D%A5%E4%BF%9D%E6%8A%A4,%E4%B8%BA0%2C%20%E7%BA%BF%E7%A8%8B%E8%BF%9B%E5%85%A5%E4%BC%91%E7%9C%A0%E3%80%82
---
+++ ThreadDemo.java
@@ -0,0 +1,39 @@
+class NewThread extends Thread {
+   Thread thread;
+
+   NewThread() {
+      // 创建第二个新线程
+      thread = new Thread(this, "Demo Thread");
+      System.out.println("Child thread: " + thread);
+      thread.start(); // 开始线程
+   }
+
+   // 第二个线程入口
+   public void run() {
+      try {
+         for (int i = 5; i > 0; i--) {
+            System.out.println("Child Thread: " + i);
+            // 暂停线程
+            Thread.sleep(1000);
+         }
+      } catch (InterruptedException e) {
+         System.out.println("Child interrupted.");
+      }
+      System.out.println("Exiting child thread.");
+   }
+}
+
+public class ThreadDemo {
+   public static void main(String args[]) {
+      new NewThread(); // 创建一个新线程
+      try {
+         for (int i = 5; i > 0; i--) {
+            System.out.println("Main Thread: " + i);
+            Thread.sleep(2000);
+         }
+      } catch (InterruptedException e) {
+         System.out.println("Main thread interrupted.");
+      }
+      System.out.println("Main thread exiting.");
+   }
+}
