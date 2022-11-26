0031aac0697d2ac8598355610b09249dc2cf20e2
Tao Feng <fengtao321@users.noreply.github.com>
2022-11-26 15:26:38 -0500

    sparse table - 1493

--- 1493-maximum-score-of-a-good-subarray-sparse-table.java
+++ 1493-maximum-score-of-a-good-subarray-sparse-table.java
@@ -1,37 +1,66 @@
-// https://www.youtube.com/watch?v=ONuuGA8U_iM
+import java.util.Arrays;
 
 class Solution {
     int[][] sparseTable;
 
     public int maximumScore(int[] nums, int k) {
-        int left = k;
-        int right = k;
-        int min = nums[k];
-        int ans = min;
+        int maxI = nums.length;
 
-        int rightMax = nums.length - 1;
+        sparseTable = new int[maxI][maxI];
 
-        while (left > 0 || right < rightMax) {
-            if (left > 0 && right < rightMax) { // we can move either left or right
-                if (nums[left - 1] > nums[right + 1]) { // <--- move to a bigger num, for example [3,7,4], we can either
-                                                        // get [3,7] or [7,4], same length, but always choose the size
-                                                        // with larger number to move
-                    left--;
-                    min = Math.min(min, nums[left]);
-                } else { // right +1 is larger or equal to left - 1
-                    right++;
-                    min = Math.min(min, nums[right]);
-                }
-            } else if (left > 0) { // only left can be moved, right==rightMax
-                left--;
-                min = Math.min(min, nums[left]);
-            } else { // only right can be moved, left==0
-                right++;
-                min = Math.min(min, nums[right]);
-            }
-            ans = Math.max(ans, min * (right - left + 1));
+        // build sparse table
+        for (int i = 0; i < maxI; i++) {
+            sparseTable[i][0] = nums[i];
         }
 
-        return ans; // left = 0, right = rightMax
+        for (int j = 1; (1 << j) < maxI; j++) {
+            for (int i = 0; (i + (1 << j) - 1) < maxI; i++) {
+                sparseTable[i][j] = Math.min(sparseTable[i][j - 1], sparseTable[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+
+        // print2D(sparseTable);
+
+        // start to find value from ths sparse table
+        int max = nums[k];
+        int left = k;
+        int right = k;
+
+        while (left > 0 || right < nums.length - 1) {
+            if (left > 0 && right < (nums.length - 1)) {
+                if (nums[left - 1] > nums[right + 1]) {
+                    left--;
+                } else {
+                    right++;
+                }
+            } else if (left > 0) {
+                left--;
+            } else {
+                right++;
+            }
+
+            max = Math.max(max, query(left, right));
+        }
+
+        return max;
+    }
+
+    private int query(int left, int right) {
+        int length = right - left + 1;
+        int j = (int) (Math.log(length) / Math.log(2));
+        // int j = Integer.numberOfTrailingZeros(Integer.highestOneBit(length));
+        int i1 = left;
+        int i2 = right - (1 << j) + 1; // faster than int i2 = right - (int) Math.pow(2, j);
+        int ans = Math.min(sparseTable[i1][j], sparseTable[i2][j]) * length;
+        return ans;
+    }
+
+    private void print2D(int mat[][]) {
+        // Loop through all rows
+        for (int[] row : mat)
+
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
     }
 }
