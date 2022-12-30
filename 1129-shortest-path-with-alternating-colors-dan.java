c6ed26d3b96b77c6d9ef7a75fda8230a1b54da8f
Tao Feng <fengtao321@users.noreply.github.com>
2022-12-29 23:56:46 -0500

    use bfs instead dijkstra in graph

--- 1129-shortest-path-with-alternating-colors-dan.java
+++ 1129-shortest-path-with-alternating-colors-dan.java
@@ -1,11 +1,10 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
-//use set to keep visited node
 class Solution {
 
     HashMap<Integer, HashSet<Integer>>[] graph;
@@ -21,37 +20,44 @@ class Solution {
         populateGraph(redEdges, 0);
         populateGraph(blueEdges, 1);
 
-        // node, cost, color
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        // node, color
+        Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[n][2];
 
         for (Map.Entry<Integer, HashSet<Integer>> entry : graph[0].entrySet()) {
             int node = entry.getKey();
             for (int color : entry.getValue()) {
-                pq.offer(new int[] { node, 1, color });
+                queue.offer(new int[] { node, color });
             }
         }
 
-        while (!pq.isEmpty()) {
-            int[] tuple = pq.poll();
-            int node = tuple[0];
-            int cost = tuple[1];
-            int color = tuple[2];
+        int level = 0;
+        while (!queue.isEmpty()) {
 
-            if (ans[node] == -1 || cost < ans[node])
-                ans[node] = cost;
+            level++;
 
-            if (visited[node][color])
-                continue;
-            visited[node][color] = true;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] tuple = queue.poll();
+                int node = tuple[0];
+                int color = tuple[1];
 
-            for (Map.Entry<Integer, HashSet<Integer>> entry : graph[node].entrySet()) {
-                int nextNode = entry.getKey();
-                for (int nextColor : entry.getValue()) {
-                    if (!visited[nextNode][nextColor] && color != nextColor)
-                        pq.offer(new int[] { nextNode, cost + 1, nextColor });
+                if (ans[node] == -1)
+                    ans[node] = level;
+
+                if (visited[node][color])
+                    continue;
+                visited[node][color] = true;
+
+                for (Map.Entry<Integer, HashSet<Integer>> entry : graph[node].entrySet()) {
+                    int nextNode = entry.getKey();
+                    for (int nextColor : entry.getValue()) {
+                        if (!visited[nextNode][nextColor] && color != nextColor)
+                            queue.offer(new int[] { nextNode, nextColor });
+                    }
                 }
             }
+
         }
 
         return ans;
--- 1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 
+// if the cost of edges is always 1, a regular bfs will be faster than using a priorityQueue (since we avoid the sorting overhead)
 class Solution {
     public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
         int[] ans = new int[n];
@@ -15,18 +16,20 @@ class Solution {
         }
 
         for (int[] edge : redEdges) {
-            map.get(edge[0]).add(new Node(edge[1], 0, -1));
+            map.get(edge[0]).add(new Node(edge[1], 0));
         }
 
         for (int[] edge : blueEdges) {
-            map.get(edge[0]).add(new Node(edge[1], 1, -1));
+            map.get(edge[0]).add(new Node(edge[1], 1));
         }
 
         Deque<Node> qDeque = new ArrayDeque<>();
-        qDeque.add(new Node(0, 0, 0));// red
-        qDeque.add(new Node(0, 1, 0)); // blue
+        qDeque.add(new Node(0, 0));// red
+        qDeque.add(new Node(0, 1)); // blue
 
-        while (!qDeque.isEmpty()) {
+        int level = 1;
+        int qDequeL = qDeque.size();
+        while (qDequeL > 0) {
             Node from = qDeque.poll();
             Deque<Node> list = map.get(from.id);
 
@@ -34,14 +37,19 @@ class Solution {
             for (int i = 0; i < listSize; i++) {
                 Node to = list.poll();
                 if (to.color != from.color) {
-                    to.path = from.path + 1;
                     qDeque.add(to);
-                    if (ans[to.id] == -1 || ans[to.id] > to.path) {
-                        ans[to.id] = to.path;
+                    if (ans[to.id] == -1) {
+                        ans[to.id] = level;
                     }
                 } else {
                     list.add(to);
                 }
+            }
+
+            qDequeL--;
+            if (qDequeL == 0) {
+                level++;
+                qDequeL = qDeque.size();
             }
         }
         return ans;
@@ -51,11 +59,9 @@ class Solution {
 class Node {
     int id;
     int color; // 0 is red, 1 is blue
-    int path;
 
-    Node(int id, int color, int path) {
+    Node(int id, int color) {
         this.id = id;
         this.color = color;
-        this.path = path;
     }
 }
