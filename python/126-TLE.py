d8f5c994f7e938b3db9d6b55ee6326edeeab7f66
Tao Feng <fengtao321@users.noreply.github.com>
2024-03-31 22:09:49 -0400

    TLE :(

---
+++ python/126-TLE.py
@@ -0,0 +1,86 @@
+# Don't use bfs only and compute the paths during bfs. Instead, only use bfs to find the length of the shortest path, and then use dfs to output the shortest paths. Otherwise you may encounter exponentially many "potential" shortest paths, but only one of them is the real solution.
+# Bidirectional-bfs codes fail for similar reasons.
+
+# When using dfs to output the shortest paths, start from endWord instead of beginWord. Otherwise the running time can easily become exponential. This is because there are exponentially many dfs branches, but only one branch can give you the shortest path.
+# Usually people perform bfs starting from beginWord. Then for each intermediate string, you know the shortest distance from it to beginWord, but don't know the shortest distance from it to endWord. If we perform dfs from endWord, we can discard those partial paths that cannot be extended into a shortest path, judging from the shortest distance from the current node to beginWord.
+# (Starting dfs from beginWord could also work, but you need to carefully avoid those partial paths that cannot be extended into a shortest path.)
+
+
+
+from collections import defaultdict, deque
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        def init():
+            hash_map = defaultdict(set)
+            for word in wordList:
+                for j in range(len(word)):
+                    key = word[0:j] + "*" + word[j+1:]
+                    hash_map[key].add(word)
+            return hash_map
+        
+        def bfs():
+            q = deque([beginWord]) #only last word
+            depth = 0
+            visited[depth] = {beginWord}
+            pre = set()
+            
+            while q:
+                length = len(q)
+                depth+=1
+                visited[depth] = set()
+                
+                for _ in range(length):
+                    word  = q.popleft()
+                    if word == endWord: return depth
+                    if word in pre: continue
+                         
+                    visited[depth].add(word)
+                    for j in range(len(word)):
+                        key = word[0:j] + "*" + word[j+1:]
+                        if key not in hash_map: continue
+                        
+                        for nxt in hash_map[key]:
+                            if nxt in visited[depth-1] or nxt in visited[depth]: continue 
+                            q.append(nxt)
+                pre.update(visited[depth-1])
+            return depth
+        
+        def dfs(l, depth):
+            word = l[-1]
+            if depth == 0:
+                if word==beginWord: ans.append(l[::-1])
+                return
+            
+            
+            for nxt in visited[depth]:
+                diff = 0
+                
+                for i in range(len(nxt)):
+                    if nxt[i]!=word[i]: diff+=1
+                    if diff>1: break
+
+                if diff==1:
+                    l.append(nxt)
+                    dfs(l, depth-1)
+                    l.pop()
+            return
+
+        hash_map = init()
+        visited = {}
+        depth = bfs()
+        # print(depth, visited)
+        ans = []
+        dfs([endWord], depth-1)
+        return ans
+
+    
+    
+# "aaaaa"
+# "ggggg"
+# ["aaaaa","caaaa","cbaaa","daaaa","dbaaa","eaaaa","ebaaa","faaaa","fbaaa","gaaaa","gbaaa","haaaa","hbaaa","iaaaa","ibaaa","jaaaa","jbaaa","kaaaa","kbaaa","laaaa","lbaaa","maaaa","mbaaa","naaaa","nbaaa","oaaaa","obaaa","paaaa","pbaaa","bbaaa","bbcaa","bbcba","bbdaa","bbdba","bbeaa","bbeba","bbfaa","bbfba","bbgaa","bbgba","bbhaa","bbhba","bbiaa","bbiba","bbjaa","bbjba","bbkaa","bbkba","bblaa","bblba","bbmaa","bbmba","bbnaa","bbnba","bboaa","bboba","bbpaa","bbpba","bbbba","abbba","acbba","dbbba","dcbba","ebbba","ecbba","fbbba","fcbba","gbbba","gcbba","hbbba","hcbba","ibbba","icbba","jbbba","jcbba","kbbba","kcbba","lbbba","lcbba","mbbba","mcbba","nbbba","ncbba","obbba","ocbba","pbbba","pcbba","ccbba","ccaba","ccaca","ccdba","ccdca","cceba","cceca","ccfba","ccfca","ccgba","ccgca","cchba","cchca","cciba","ccica","ccjba","ccjca","cckba","cckca","cclba","cclca","ccmba","ccmca","ccnba","ccnca","ccoba","ccoca","ccpba","ccpca","cccca","accca","adcca","bccca","bdcca","eccca","edcca","fccca","fdcca","gccca","gdcca","hccca","hdcca","iccca","idcca","jccca","jdcca","kccca","kdcca","lccca","ldcca","mccca","mdcca","nccca","ndcca","occca","odcca","pccca","pdcca","ddcca","ddaca","ddada","ddbca","ddbda","ddeca","ddeda","ddfca","ddfda","ddgca","ddgda","ddhca","ddhda","ddica","ddida","ddjca","ddjda","ddkca","ddkda","ddlca","ddlda","ddmca","ddmda","ddnca","ddnda","ddoca","ddoda","ddpca","ddpda","dddda","addda","aedda","bddda","bedda","cddda","cedda","fddda","fedda","gddda","gedda","hddda","hedda","iddda","iedda","jddda","jedda","kddda","kedda","lddda","ledda","mddda","medda","nddda","nedda","oddda","oedda","pddda","pedda","eedda","eeada","eeaea","eebda","eebea","eecda","eecea","eefda","eefea","eegda","eegea","eehda","eehea","eeida","eeiea","eejda","eejea","eekda","eekea","eelda","eelea","eemda","eemea","eenda","eenea","eeoda","eeoea","eepda","eepea","eeeea","ggggg","agggg","ahggg","bgggg","bhggg","cgggg","chggg","dgggg","dhggg","egggg","ehggg","fgggg","fhggg","igggg","ihggg","jgggg","jhggg","kgggg","khggg","lgggg","lhggg","mgggg","mhggg","ngggg","nhggg","ogggg","ohggg","pgggg","phggg","hhggg","hhagg","hhahg","hhbgg","hhbhg","hhcgg","hhchg","hhdgg","hhdhg","hhegg","hhehg","hhfgg","hhfhg","hhigg","hhihg","hhjgg","hhjhg","hhkgg","hhkhg","hhlgg","hhlhg","hhmgg","hhmhg","hhngg","hhnhg","hhogg","hhohg","hhpgg","hhphg","hhhhg","ahhhg","aihhg","bhhhg","bihhg","chhhg","cihhg","dhhhg","dihhg","ehhhg","eihhg","fhhhg","fihhg","ghhhg","gihhg","jhhhg","jihhg","khhhg","kihhg","lhhhg","lihhg","mhhhg","mihhg","nhhhg","nihhg","ohhhg","oihhg","phhhg","pihhg","iihhg","iiahg","iiaig","iibhg","iibig","iichg","iicig","iidhg","iidig","iiehg","iieig","iifhg","iifig","iighg","iigig","iijhg","iijig","iikhg","iikig","iilhg","iilig","iimhg","iimig","iinhg","iinig","iiohg","iioig","iiphg","iipig","iiiig","aiiig","ajiig","biiig","bjiig","ciiig","cjiig","diiig","djiig","eiiig","ejiig","fiiig","fjiig","giiig","gjiig","hiiig","hjiig","kiiig","kjiig","liiig","ljiig","miiig","mjiig","niiig","njiig","oiiig","ojiig","piiig","pjiig","jjiig","jjaig","jjajg","jjbig","jjbjg","jjcig","jjcjg","jjdig","jjdjg","jjeig","jjejg","jjfig","jjfjg","jjgig","jjgjg","jjhig","jjhjg","jjkig","jjkjg","jjlig","jjljg","jjmig","jjmjg","jjnig","jjnjg","jjoig","jjojg","jjpig","jjpjg","jjjjg","ajjjg","akjjg","bjjjg","bkjjg","cjjjg","ckjjg","djjjg","dkjjg","ejjjg","ekjjg","fjjjg","fkjjg","gjjjg","gkjjg","hjjjg","hkjjg","ijjjg","ikjjg","ljjjg","lkjjg","mjjjg","mkjjg","njjjg","nkjjg","ojjjg","okjjg","pjjjg","pkjjg","kkjjg","kkajg","kkakg","kkbjg","kkbkg","kkcjg","kkckg","kkdjg","kkdkg","kkejg","kkekg","kkfjg","kkfkg","kkgjg","kkgkg","kkhjg","kkhkg","kkijg","kkikg","kkljg","kklkg","kkmjg","kkmkg","kknjg","kknkg","kkojg","kkokg","kkpjg","kkpkg","kkkkg","ggggx","gggxx","ggxxx","gxxxx","xxxxx","xxxxy","xxxyy","xxyyy","xyyyy","yyyyy","yyyyw","yyyww","yywww","ywwww","wwwww","wwvww","wvvww","vvvww","vvvwz","avvwz","aavwz","aaawz","aaaaz"]
+                    
+        
+        
+        
